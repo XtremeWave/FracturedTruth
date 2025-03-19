9cec68b841583ddfabcbd268d28aa1a20d3e2c87
NewLezaiYa <3651440375@qq.com>
2025-03-19 21:43:32 +0800

    添加全局using

--- FinalSuspect/GlobalUsings.cs
+++ FinalSuspect/GlobalUsings.cs
@@ -7,3 +7,7 @@ global using FinalSuspect.Modules.Core.Plugin;
 global using HarmonyLib;
 global using static FinalSuspect.Modules.Core.Plugin.Translator;
 global using static FinalSuspect.Modules.Resources.PathManager;
+global using static FinalSuspect.DataHandling.XtremeGameData.GameStates;
+global using static FinalSuspect.DataHandling.FinalAntiCheat.FAC;
+global using static FinalSuspect.Modules.Core.Plugin.XtremeLogger;
+global using static FinalSuspect.Modules.Core.Game.Utils;
