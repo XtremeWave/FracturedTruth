3598be2027f484b26df08ff5d9ce7dc917b24a4a
NewLezaiYa <3651440375@qq.com>
2025-05-15 17:15:31 +0800

    代码优化

--- FinalSuspect/DataHandling/FinalAntiCheat/Core/DllChecker.cs
+++ FinalSuspect/DataHandling/FinalAntiCheat/Core/DllChecker.cs
@@ -9,43 +9,32 @@ public static class DllChecker
 {
     private static void Prefix(MainMenuManager __instance)
     {
-        // 针对基于BepInEx注入检测
+        // SM的文件名是写死的
+        string[] suspiciousFiles = { "SickoMenu.dll", "version.dll" };
         // 获取当前Dll启动目录
         string directoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-        // 检查该目录下的所有文件
+        // 获取游戏根目录
+        string rootPath = Environment.CurrentDirectory;
+        // 针对基于BepInEx注入检测
         foreach (string path in Directory.EnumerateFiles(directoryPath, "*.*"))
         {
             string fileName = Path.GetFileName(path);
 
-            // 检查是否是非目标文件
             if (fileName != "FinalSuspect.dll")
             {
-                // 记录日志
                 Error($"检测到非法文件: {fileName}！游戏将被强制终止。", "FAC");
-
-                // 终止游戏运行
                 Environment.Exit(1);
             }
         }
 
-        // 基于version注入检测
-        string rootPath = Environment.CurrentDirectory;
-
-        // SM的文件名是写死的
-        string[] suspiciousFiles = { "SickoMenu.dll", "version.dll" };
-
-        // 检查根目录每个文件是否存在该文件
+        // 针对基于version注入检测
         foreach (string fileName in suspiciousFiles)
         {
             string fullPath = Path.Combine(rootPath, fileName);
 
             if (File.Exists(fullPath))
             {
-                // 记录日志
                 Error($"检测到非法文件: {fileName}！游戏将被强制终止。", "FAC");
-
-                // 终止游戏运行
                 Environment.Exit(1);
             }
         }
